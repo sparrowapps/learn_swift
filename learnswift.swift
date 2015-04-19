1aba6366383370bdf71b6e60b6232b39a40ab6d8
sparrowapps <mt.sparrow@gmail.com>
2015-04-19 22:50:33 +0900

    initial commit

---
+++ learnswift.swift
@@ -0,0 +1,46 @@
+// 스위프트 언어에만 있는 특징 적인 것만을 기술 한다.
+
+/*
+1. 스위프트 옵셔널 타입
+선언 시에 ?
+참조 할때 !
+새로운 상수를 만들어 할당 하고 상수로 참조
+암묵적인 언래핑을 위해 선언시 !
+
+*/
+import UIKit
+
+var index: Int?
+
+index = 3
+
+var treeArray = ["ok", "pine", "Yew", "Brich"]
+
+
+if let myValue = index {
+    println(treeArray[myValue])
+    
+}else {
+    println("index does not contain a value")
+}
+
+/*
+2. 스위프트튜플
+let myTuple = (10, 11.33, "string")
+
+*/
+
+var myString = myTuple.2
+
+// 모두 추출
+let (myInt, myFloat, myString) = myTuple
+
+// 무시
+
+var ( myInt, _, myString) = myTuple
+
+let myTuple = ( count: 10, length : 11.33, message : "string" )
+
+println (myTuple.message)
+
+
