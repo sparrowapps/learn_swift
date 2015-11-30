b708a43a0ad29896ec0a344d4b35972c93958bd7
sparrowapps <mt.sparrow@gmail.com>
2015-12-01 08:02:16 +0900

    Update function.swift

--- function.swift
+++ function.swift
@@ -1,1 +1,44 @@
+standford ituneu에서 나온 코드다.
+이게 좀 이해가 가지 않았는데.
+이 일단 이형태를 기록으로 둔다.
+
+
 #function
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+        case "×": performOperation {$0 * $1}
+        case "÷": performOperation {$1 / $0}
+        case "+": performOperation {$0 + $1}
+        case "-": performOperation {$1 - $0}
+        default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        print("operandStack = \(operandStack)")
+        
+    }
+    
+    //computed property
+    var displayValue : Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
