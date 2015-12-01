308351fedcf3443362dc2ebcc579f178dbc5691b
sparrowapps <mt.sparrow@gmail.com>
2015-12-01 09:21:33 +0900

    Update function.swift
    swift 2.0 function overloading private keyword needed

--- function.swift
+++ function.swift
@@ -42,3 +42,43 @@ standford ituneu에서 나온 코드다.
             userIsInTheMiddleOfTypingANumber = false
         }
     }
+
+
+
+
+함수 오버로딩
+swift 1.2 버젼에서
+swift 2.0으로 바뀐점인듯 하다.
+
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+        case "×": performOperation { $0 * $1 }
+        case "÷": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "-": performOperation { $1 - $0 }
+        case "√": performOperation { sqrt($0) }
+        default: break
+        }
+    }
+
+    private func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+
+    private func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+
+여기서 private 키워드 없이 하면  2.0에서는 에러가 발생 한다.
+관련 내용
+http://stackoverflow.com/questions/29457720/compiler-error-method-with-objective-c-selector-conflicts-with-previous-declara
