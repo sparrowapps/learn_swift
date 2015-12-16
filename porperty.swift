5aff7e7d601083d51046aaffae53e1395623f888
sparrowapps <mt.sparrow@gmail.com>
2015-12-16 23:47:56 +0900

    Create porperty.swift
    init

---
+++ porperty.swift
@@ -0,0 +1,9 @@
+var someStoredProperty: Int = 2 {
+  willSet { newVale is the new value }
+  didSet { oldValue is the old value }
+  }
+  
+  override var ihheritedProperty {
+    willSet { newValue is the new value }
+    didSet { oldValue is the old value }
+    }
