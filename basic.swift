61b54fdfbf3f15bbfbcd7b43028165c86aa947ec
sparrowapps <mt.sparrow@gmail.com>
2016-04-17 22:06:07 +0900

    swift  기본, 타입 정리 중.

--- basic.swift
+++ basic.swift
@@ -1,1 +1,34 @@
-# operator, operand, assignment
+// operator, operand, assignment
+
+// 타입엘리어스
+
+typealias NameType = String
+typealias IdType = UInt32
+
+var customerId :IdType
+var customerName :NameType
+
+
+// 튜풀
+
+var x = 5
+var y = 4
+
+var point = (7,8)
+
+var point: (Int, Int)
+point = (7,8)
+
+
+var pbook  = ("junho", "123-1234-1234")
+let (name, num) = pbook
+
+//튜풀 값 가져 오기
+var pbook  = ("junho", "123-1234-1234")
+let (name, num) = pbook
+
+print(name)
+print(num)
+// 결과는 아래와 같다.
+//"junho\n"
+//"123-1234-1234\n"
