32ca5e7aaddbf294ebbb6e3a1e03bbab489cccb8
sparrowapps <mt.sparrow@gmail.com>
2016-04-24 22:48:28 +0900

    Update clouser.swift
    map, filter function example add

--- clouser.swift
+++ clouser.swift
@@ -31,3 +31,34 @@ print(reversed)
  }
  
  */
+
+// map example
+let digitNames = [
+    0: "Zero", 1: "One", 2: "Two",   3: "Three", 4: "Four",
+    5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"
+]
+let numbers = [16, 58, 510]
+
+let strings = numbers.map {
+    (number) -> String in
+    var number = number
+    var output = ""
+    while number > 0 {
+        output = digitNames[number % 10]! + output
+        number /= 10
+    }
+    return output
+}
+
+print(strings)
+
+
+// filter example
+let prices = [12.0, 15.2, 30.1, 55,2]
+var priceOver20  = prices.filter {
+    (price:Double) -> Bool in
+        price>20
+}
+
+print (priceOver20)
+
