00474b2a5b326f20992d20ec86d7e756280fc150
sparrowapps <mt.sparrow@gmail.com>
2016-04-24 22:39:41 +0900

    Update clouser.swift

--- clouser.swift
+++ clouser.swift
@@ -1,1 +1,33 @@
 #clouser
+
+import UIKit
+
+let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+func backwards(s1: String, _ s2: String) -> Bool {
+    return s1 > s2
+}
+
+var reversed = names.sort(backwards)
+
+print(reversed)
+
+
+reversed = names.sort({ (s1: String, s2: String) -> Bool in
+    return s1 > s2
+})
+
+print(reversed)
+
+reversed = names.sort( { $0 > $1 } )
+print(reversed)
+
+reversed = names.sort(>)
+print(reversed)
+
+
+/*
+ { (parameters) -> return type in
+ statements
+ }
+ 
+ */
